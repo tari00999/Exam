1b2e1cccdbbd879c5b8dce0b045593f9582e5407
sibasakiharuki <tks2573481@stu.o-hara.ac.jp>
2026-04-27 14:06:01 +0900

    入れ忘れ

--- src/main/java/bean/Score.java
+++ src/main/java/bean/Score.java
@@ -2,34 +2,4 @@ package bean;
 
 public class Score {
 
-    private String studentNo; // 学生番号
-    private String subject;   // 科目
-    private int score;        // 点数
-
-    // 学生番号
-    public String getStudentNo() {
-        return studentNo;
-    }
-
-    public void setStudentNo(String studentNo) {
-        this.studentNo = studentNo;
-    }
-
-    // 科目
-    public String getSubject() {
-        return subject;
-    }
-
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
-
-    // 点数
-    public int getScore() {
-        return score;
-    }
-
-    public void setScore(int score) {
-        this.score = score;
-    }
 }
