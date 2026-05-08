1382a8ff54fc73dbb6daa4ff1df5707133ca73de
tari00999 <tks2573514@stu.o-hara.ac.jp>
2026-05-08 12:29:32 +0900

    クラス管理

--- src/main/java/scoremanager/main/ClassUpdateAction.java
+++ src/main/java/scoremanager/main/ClassUpdateAction.java
@@ -1,5 +1,25 @@
 package scoremanager.main;
 
-public class ClassUpdateAction {
+import bean.School;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import tool.Action;
+
+public class ClassUpdateAction extends Action {
+
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		//セッション
+		HttpSession session =request.getSession();
+		
+		//学生情報
+		School school=(school)session.getAttribute("school");
+		
+		//パラメータ
+		
+	}
 
 }
